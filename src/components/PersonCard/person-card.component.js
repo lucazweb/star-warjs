e77fbfcf726142540e2910ef82a428def2118318
lucazweb <lucazwebmail@gmail.com>
2020-03-31 16:40:24 -0300

    Add navigation sketch between pages

--- src/components/PersonCard/person-card.component.js
+++ src/components/PersonCard/person-card.component.js
@@ -1,32 +1,42 @@
 import React from "react";
 import { Card } from "./person-card.styled";
-export const PersonCard = () => (
-  <Card>
-    <div className="card-body">
-      <div className="person-image"></div>
-      <div className="person-info">
-        <ul>
-          <li>
-            <strong>Height</strong>
-            <span>177</span>
-          </li>
-          <li>
-            <strong>Mass</strong>
-            <span>177</span>
-          </li>
-          <li>
-            <strong>Hair color</strong>
-            <span>Blond</span>
-          </li>
-          <li>
-            <strong>Skin color</strong>
-            <span>Fair</span>
-          </li>
-        </ul>
+import { useHistory } from 'react-router-dom';
+
+export const PersonCard = () => {
+
+  const history = useHistory();
+  const selectPerson = () => {
+    history.push('/details/1');
+  }
+
+  return (
+    <Card onClick={() => selectPerson()}>
+      <div className="card-body">
+        <div className="person-image"></div>
+        <div className="person-info">
+          <ul>
+            <li>
+              <strong>Height</strong>
+              <span>177</span>
+            </li>
+            <li>
+              <strong>Mass</strong>
+              <span>177</span>
+            </li>
+            <li>
+              <strong>Hair color</strong>
+              <span>Blond</span>
+            </li>
+            <li>
+              <strong>Skin color</strong>
+              <span>Fair</span>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-    <div className="card-footer">
-      <span>Luke Skywalker</span>
-    </div>
-  </Card>
-);
+      <div className="card-footer">
+        <span>Luke Skywalker</span>
+      </div>
+    </Card>
+  )
+};
